c47876b5b9b2f13b456370007aa99fea693748fe
alexandresalgueiro <alexandre.salgueirohenriques@gmail.com>
2023-12-02 17:41:20 +0100

    if inutile

--- bibli.py
+++ bibli.py
@@ -11,37 +11,34 @@ class bibli(Simple_bibli):
             try:
                 # Vérifie si le livre est déjà présent
                 nom_fichier = os.path.basename(lien)
-                if nom_fichier in self.livres:  # attention ca marche pas ?
-                    print(f"Le livre {nom_fichier} est déjà présent.")
-                else:
-                    # Envoie une requête GET pour récupérer le contenu du fichier
-                    response = requests.get(lien, stream=True, verify=False)
+                # Envoie une requête GET pour récupérer le contenu du fichier
+                response = requests.get(lien, stream=True, verify=False)
 
-                    # Vérifie si la requête a réussi (code 200 OK)
-                    if response.status_code == 200:
-                        # Récupère le nom du fichier depuis l'URL
-                        nom_fichier = os.path.basename(lien)
-                        # Enregistre le fichier dans le dossier local
-                        chemin_local = os.path.join(self.path, nom_fichier)
-                        with open(chemin_local, 'wb') as fichier_local:
-                            shutil.copyfileobj(response.raw, fichier_local)
-                        extension = os.path.splitext(nom_fichier)[1].lower()
-                        match extension:
-                            case '.epub':
-                                livre = Livre_EPUB(os.path.join(self.path, nom_fichier))
-                                self.livres.append(livre)
-                                self.ajoute_auteur(livre)
-                            case '.pdf':
-                                livre = Livre_PDF(os.path.join(self.path, nom_fichier))
-                                self.livres.append(livre)
-                                self.ajoute_auteur(livre)
-                            case _:
-                                raise Exception("Extension pas prise en compte")
-                        print(f"Le livre {nom_fichier} a été téléchargé et enregistré.")
-                        return True
-                    else:
-                        print(f"Échec de la requête avec le code d'état : {response.status_code}")
-                        return False
+                # Vérifie si la requête a réussi (code 200 OK)
+                if response.status_code == 200:
+                    # Récupère le nom du fichier depuis l'URL
+                    nom_fichier = os.path.basename(lien)
+                    # Enregistre le fichier dans le dossier local
+                    chemin_local = os.path.join(self.path, nom_fichier)
+                    with open(chemin_local, 'wb') as fichier_local:
+                        shutil.copyfileobj(response.raw, fichier_local)
+                    extension = os.path.splitext(nom_fichier)[1].lower()
+                    match extension:
+                        case '.epub':
+                            livre = Livre_EPUB(os.path.join(self.path, nom_fichier))
+                            self.livres.append(livre)
+                            self.ajoute_auteur(livre)
+                        case '.pdf':
+                            livre = Livre_PDF(os.path.join(self.path, nom_fichier))
+                            self.livres.append(livre)
+                            self.ajoute_auteur(livre)
+                        case _:
+                            raise Exception("Extension pas prise en compte")
+                    print(f"Le livre {nom_fichier} a été téléchargé et enregistré.")
+                    return True
+                else:
+                    print(f"Échec de la requête avec le code d'état : {response.status_code}")
+                    return False
 
             except Exception as e:
                 print(f"Une erreur s'est produite lors du téléchargement du livre {lien} : {e}")
@@ -77,3 +74,7 @@ class bibli(Simple_bibli):
         except Exception as e:
             print(f"Une erreur s'est produite : {e}")
             return None
+
+B1 = bibli("biblio")
+B1.alimenter("https://math.univ-angers.fr/~jaclin/biblio/livres/", 10)
+print(B1)
